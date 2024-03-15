836ac44201eb1099470730b799faa92b54c8d03d
Maryjanesilva <ribeirodasilvam083@gmail.com>
2024-03-15 15:41:35 -0300

    exemplo de condiçao

--- routes/api.php
+++ routes/api.php
@@ -3,6 +3,15 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/user', function (Request $request) {
-    return $request->user();
-})->middleware('auth:sanctum');
+Route::get('exemplo/condicao', function (Request $request){
+$idade = $request->input('idade');
+$mesage = "";
+if($idade >= 18){
+$mesage = "Maior de idade";
+}
+else{
+    $mesage = "Menor de idade";
+}
+return $mesage;
+
+});
