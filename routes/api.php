569c87288b5f3f29e22fa69db2a20757ba006b3d
Maryjanesilva <ribeirodasilvam083@gamil.com>
2024-03-22 15:44:44 -0300

    ativ avaliativa

--- routes/api.php
+++ routes/api.php
@@ -3,7 +3,7 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
-Route::get('exemplo/condicao', function (Request $request){
+Route::get('questao 3', function (Request $request){
 $idade = $request->input('idade');
 $mesage = "";
 if($idade >= 18){
@@ -27,7 +27,7 @@ if ($idade > 18){
 }
 });
 
-Route:: get('verificar/par',function(Request $request){
+Route:: get('questao 4',function(Request $request){
 $numero = $request->input('numero');
 if($numero % 2 == 0){
 return "par";
@@ -36,7 +36,7 @@ return "par";
 }
 });
 
-Route::get('verificar/numero',function(Request $request){
+Route::get('questao 1',function(Request $request){
 $numero = $request->input('numero');
 if($numero >=10){
     return 'maior numero';
@@ -45,16 +45,16 @@ if($numero >=10){
 }
 });
 
-Route::get('verificar/temperatura',function(Request $request){
+Route::get('questao 7',function(Request $request){
 $temperatura = $request->input('temperatura');
-if($temperatura >30){
+if($temperatura >=30){
 return 'esta quente';
 }else{
 return 'nao esta quente';
 }
 });
 
-Route::get('verificar/numero',function(Request $request){
+Route::get('questao 2',function(Request $request){
 $numero =$request->input('numero');
 if($numero>0){
     return 'este numero e positivo';
@@ -64,7 +64,7 @@ return 'este numero e negativo';
     return 'este numero e zero';
 }
 });
-Route::get('verificar/numero',function(Request $request){
+Route::get('questao 5',function(Request $request){
 $numero1=$request->input('numero1');
 $numero2=$request->input('numero2');
 if($numero1 > $numero2){
@@ -80,7 +80,65 @@ $resto=($numero%3);
 if($numero ==0){
 return 'numero e divisivel';
 }else{
-    return ''
+    return '';
+}
 
+});
+
+Route::get('questao 6',function(Request $request){
+$numero=$request->input('numero');
+$resto=($numero%9);
+if($numero==0){
+    return 'o numero e divisivel';
+}else{
+    return 'o numero nao e divisivel';
 }
 });
+
+Route::get('questao 9',function(Request $request){
+$idade=$request->input('idade');
+if($idade<12){
+    return 'voce e uma crianca';
+}else{
+    return 'voce nao e uma criança';
+}
+});
+
+Route::get('questao 12',function(Request $request){
+$numero=$request->input('numero');
+$resto=($numero%6);
+if($numero==0){
+    return 'o numero e divisivel';
+}else{
+    return 'o numero nao e divisivel';
+}
+});
+
+Route::get('questao 13',function(Request $request){
+$nome=$request->input('nome');
+if($nome){
+    return 'nome e alice';
+}else{
+    return 'ola alice';
+}
+});
+
+Route::get('questao 8',function(Request $request){
+$numero=$request->input('numero');
+$resto=($numero%2);
+if($numero==7){
+    return 'o numero e multiplo de 7';
+}else{
+return 'o numero nao e multiplo de 7';
+}
+});
+
+Route::git('questao 14',function(Request $request){
+$idade=$request->input('idade');
+if($idade>=18){
+    return 'se ele tiver carta e for maior de 18 pode dirigir';
+}else{
+    return 'se ele nao tiver carta e nao for maior de 18 nao pode dirigir';
+}
+});
+
